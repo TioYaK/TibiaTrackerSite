9383337876e04639a8bbe7a2bd6247ac1a64ef6f
YaK <pifot16@gmail.com>
2026-07-12 17:03:17 -0300

    fix: Include termos.html in Vite build config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite'
 
+import { resolve } from 'path'
+
 export default defineConfig({
   base: '/TibiaTrackerSite/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        termos: resolve(__dirname, 'termos.html')
+      }
+    }
+  }
 })
